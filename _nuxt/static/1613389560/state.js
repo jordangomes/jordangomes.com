7e083e1feaa820205b0cade8c7176d9e18aaec8c
jordangomes <jordangomes@users.noreply.github.com>
2021-02-15 11:46:08 +0000

    deploy: a588a99eb8cbda316a4db7488a7a64ecb3a7007a

---
+++ _nuxt/static/1613389560/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1613389560",layout:"default",error:a,serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d93f01db"}}}}(null,"\u002F"));
